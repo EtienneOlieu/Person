31c1dd234007aad6c4b5b16085c9e8a75dc9cfa7
Horrid Entity <etienne.olieu@gmail.com>
2021-09-21 13:29:57 +0200

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,19 @@
+public class Test {
+
+    public static void main(String[] args) {
+        Person person = new Person("Thomas",9);
+
+        System.out.println(person);
+
+        person.birthDay();
+
+        System.out.println(person);
+
+        person.birthDay();
+
+        System.out.println(person);
+
+
+    }
+
+}
